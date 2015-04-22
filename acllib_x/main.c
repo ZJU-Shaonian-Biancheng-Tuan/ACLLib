249a6422af872482cddee6ef94cf41c8564c4be4
Axure <axurez@vip.qq.com>
2015-04-22 17:32:34 +0800

    Start to port ACLLib to X11.

---
+++ acllib_x/main.c
@@ -0,0 +1,18 @@
+#include "acllib.h"
+
+
+int Setup()
+{
+	initWindow("Test",DEFAULT,DEFAULT,800,600);
+	
+  	initConsole();
+  	printf("Hello\n");
+  
+	beginPaint();
+
+	line(10,10,100,100);
+
+	endPaint();
+	
+	return 0;
+}
